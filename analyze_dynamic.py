849a9eca56a40a1e8e1794d8b729730bfd43550e
Lukasz Smierzchala <lukasz.smierzchala@fenergo.com>
2022-04-03 14:17:41 +0200

    add refiting

---
+++ analyze_dynamic.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+static_mape_errors = np.load("static_MAPE.npy")
+dynamic_mape_errors = np.load("dynamic_MAPE.npy")
+
+static_mape_errors = np.mean(static_mape_errors, axis=0)
+dynamic_mape_errors = np.mean(dynamic_mape_errors, axis=0)
+
+# print(np.mean(static_mape_errors))
+# print(np.mean(dynamic_mape_errors))
+
+static_mape_errors = moving_average(static_mape_errors, n=20)
+dynamic_mape_errors = moving_average(dynamic_mape_errors, n=20)
+
+x_axis = range(len(static_mape_errors))
+
+
+plt.title("Compare MLP - without and with refitting")
+plt.plot(x_axis, static_mape_errors, label='STATIC', color='r', linewidth=1)
+plt.plot(x_axis, dynamic_mape_errors, label='DYNAMIC', color='b', linewidth=1)
+plt.xlabel('Time')
+plt.ylabel('MAPE')
+plt.legend()
+plt.show()
---
+++ predict_dynamic.py
@@ -0,0 +1,56 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_percentage_error
+import numpy as np
+
+REPEAT = 10
+
+CHUNK_SIZE = 1000
+
+X = np.load("X.npy")
+Y = np.load("Y.npy")
+
+failure_time = 16000
+
+X_train = X[:failure_time, :]
+X_test = X[failure_time:, :]
+Y_train = Y[:failure_time, :]
+Y_test = Y[failure_time:, :]
+
+Y_train = np.ravel(Y_train)
+Y_test = np.ravel(Y_test)
+
+chunks_number = int(X_test.shape[0] / 100)
+
+X_test_chunks = np.array(np.split(X_test, chunks_number))
+Y_test_chunks = np.array(np.split(Y_test, chunks_number))
+
+
+static_mape_errors = np.zeros((REPEAT, chunks_number))
+dynamic_mape_errors = np.zeros((REPEAT, chunks_number))
+
+for i in range(REPEAT):
+    # without re-fit
+    model = MLPRegressor()
+    model.fit(X_train, Y_train)
+
+    for c in range(chunks_number):
+        prediction = model.predict(X_test_chunks[c])
+        static_mape_errors[i, c] = mean_absolute_percentage_error(
+            Y_test_chunks[c], prediction)
+
+    # with re-fit
+    model = MLPRegressor()
+    model.partial_fit(X_train, Y_train)
+
+    for c in range(chunks_number):
+        prediction = model.predict(X_test_chunks[c])
+        dynamic_mape_errors[i, c] = mean_absolute_percentage_error(
+            Y_test_chunks[c], prediction)
+
+        model.partial_fit(X_test_chunks[c], Y_test_chunks[c])
+
+    print('RUN: ', i + 1, ' - static mean: ', static_mape_errors[i].mean())
+    print('RUN: ', i + 1, ' - dynamic mean: ', dynamic_mape_errors[i].mean())
+
+np.save("static_MAPE.npy", static_mape_errors)
+np.save("dynamic_MAPE.npy", dynamic_mape_errors)
